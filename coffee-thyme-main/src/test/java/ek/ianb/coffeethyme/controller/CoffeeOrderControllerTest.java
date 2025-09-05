4e598aa6d52b9d864a699840e7916f7c446ab72d
SimAbild <abildtrup123@gmail.com>
2025-09-05 10:53:14 +0200

    Lavet hele opgaven

--- coffee-thyme-main/src/test/java/ek/ianb/coffeethyme/controller/CoffeeOrderControllerTest.java
+++ coffee-thyme-main/src/test/java/ek/ianb/coffeethyme/controller/CoffeeOrderControllerTest.java
@@ -1,16 +1,25 @@
 package ek.ianb.coffeethyme.controller;
 
+import ek.ianb.coffeethyme.model.CoffeeOrder;
 import ek.ianb.coffeethyme.service.CoffeeOrderService;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.junit.jupiter.api.Assertions.*;
-@WebMvcTest
+
+@WebMvcTest(CoffeeOrderController.class)
 class CoffeeOrderControllerTest {
 
     @Autowired
@@ -31,7 +40,34 @@ class CoffeeOrderControllerTest {
     void shouldShowOrderForm() throws Exception {
         mockMvc.perform(get("/coffee"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("order-form"))
+                .andExpect(view().name("order-form"));
 
     }
+
+    @Test
+    void shouldPlaceOrder() throws Exception {
+        CoffeeOrder coffeeOrder = new CoffeeOrder("Flat White", "Medium", "Skim");
+        when(coffeeOrderService.placeOrder(any(CoffeeOrder.class))).thenReturn(coffeeOrder);
+
+        mockMvc.perform(post("/coffee/order")
+                .param("coffeeType","Flat White")
+                .param("coffeeSize","Medium")
+                .param("milkType","Skim"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/coffee/show-order"));
+
+        ArgumentCaptor<CoffeeOrder> captor = ArgumentCaptor.forClass(CoffeeOrder.class);
+        verify(coffeeOrderService).placeOrder(captor.capture());
+        //Vi laver et arguemntCaptor fordi når man laver et coffeeORder objekt vil der blive tilkoblet et OrderID på
+        // vi ved ikke hvad OrderID vil være, så man bruger ArgumentCaptor til at fange OrderID og verify det
+
+        CoffeeOrder captured = captor.getValue();
+        assertEquals("Flat White",captured.getCoffeeType());
+        assertEquals("Medium",captured.getCoffeeSize());
+        assertEquals("Skim",captured.getMilkType());
+        assertNotNull(captured.getOrderId()); //UUID
+
+    }
+
+
 }
